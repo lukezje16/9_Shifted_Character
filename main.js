0295d3b638957280f2c1a4563874fbf816b241e0
Luke Carty <lukecarty161@gmail.com>
2023-03-10 15:23:03 +0000

    chnaged the char to upper case to check first

--- main.js
+++ main.js
@@ -14,7 +14,10 @@ class ShiftCipher {
       //set the character at the index to char
       let char = string1[i];
       //change the character to lower case
-      char.toLowerCase();
+      if (char === char.toUpperCase()) {
+        //convert to lowercase if it is
+        char = char.toLowerCase();
+      }
       //log the character so i know its being set
       console.log(char);
       //check the index of the letter against the alphabet
@@ -43,8 +46,48 @@ class ShiftCipher {
 
     return shiftedString.toUpperCase();
   }
+
+  decrypt(string1) {
+    let shiftedString = "";
+    //for each letter of string
+    for (let i = 0; i < string1.length; i++) {
+      //set the character at the index to char
+      let char = string1[i];
+      //change the character to lower case
+      if (char === char.toUpperCase()) {
+        //convert to lowercase if it is
+        char = char.toLowerCase();
+      }
+      //log the character so i know its being set
+      console.log(char);
+      //check the index of the letter against the alphabet
+      let indexOfMatch = this._alphabet.indexOf(char.toLowerCase());
+
+      if (indexOfMatch !== -1) {
+        //get the index of the match and shift it by amount defined
+        console.log("before the shift:" + indexOfMatch);
+        indexOfMatch -= this._shift;
+        //log the shift change
+        console.log("after the shift: " + indexOfMatch);
+        //handle wrapping
+        indexOfMatch = (indexOfMatch + 26) % 26;
+        //set the shifted character based on the index of the newly shifted index in the alphabet
+        let shiftedCharacter = this._alphabet[indexOfMatch];
+        //log the newly shifted character
+        console.log(`newly shifted character is:  ${shiftedCharacter}`);
+        console.log("******************************************************");
+        //append the string
+        shiftedString += shiftedCharacter;
+      } else {
+        //append the string if no index of character is found
+        shiftedString += char;
+      }
+    }
+
+    return shiftedString;
+  }
 }
 
-const cipher = new ShiftCipher(15);
+const cipher = new ShiftCipher(1);
 
-console.log(cipher.encrypt("help"));
+console.log(cipher.encrypt("ABC"));
